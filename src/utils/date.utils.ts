77b5dc111b6c35f8f1194ccca4cdccea1c8e9a14
Evgenii Stulnikov <xxjenekxx@gmail.com>
2025-03-24 23:52:42 +0300

    Refactor price data preparation with hourly timestamps and add exchange rates init

--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -3,3 +3,18 @@ export function getTimestamp30DaysAgo(): number {
   date.setDate(date.getDate() - 30);
   return Math.floor(date.getTime() / 1000);
 }
+
+export function generateHoursByTimestampRange(from: number, to: number): number[] {
+  const hourInSeconds = 3600;
+  const roundedFrom = Math.ceil(from / hourInSeconds) * hourInSeconds;
+
+  const hours: number[] = [];
+  let currentHour = roundedFrom;
+
+  while (currentHour <= to) {
+    hours.push(currentHour);
+    currentHour += hourInSeconds;
+  }
+
+  return hours;
+}
